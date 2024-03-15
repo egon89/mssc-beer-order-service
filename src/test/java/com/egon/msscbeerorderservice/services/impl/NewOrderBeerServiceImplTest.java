5ec80b11951d8a41c4c973727548401277050383
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-03-15 18:11:42 -0300

    test: unit test for validate order request action

--- src/test/java/com/egon/msscbeerorderservice/services/impl/NewOrderBeerServiceImplTest.java
+++ src/test/java/com/egon/msscbeerorderservice/services/impl/NewOrderBeerServiceImplTest.java
@@ -1,14 +1,20 @@
 package com.egon.msscbeerorderservice.services.impl;
 
 import com.egon.brewery.dtos.BeerOrderDto;
+import com.egon.brewery.dtos.request.ValidateBeerOrderRequest;
 import com.egon.msscbeerorderservice.enums.OrderStatusEnum;
 import com.egon.msscbeerorderservice.repositories.BeerOrderRepository;
 import com.egon.msscbeerorderservice.services.NewOrderBeerService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jms.core.JmsTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 
 @SpringBootTest
 class NewOrderBeerServiceImplTest {
@@ -19,6 +25,10 @@ class NewOrderBeerServiceImplTest {
   @Autowired
   BeerOrderRepository repository;
 
+  // remove the jmsTemplate mock to run an integration test
+  @MockBean
+  JmsTemplate jmsTemplate;
+
   @Test
   void shouldCreateANewBeerOrder() {
     final var dto = BeerOrderDto.builder().build();
@@ -27,5 +37,6 @@ class NewOrderBeerServiceImplTest {
 
     final var beerOrder = repository.findById(savedBeerOrder.getId()).orElseThrow();
     assertThat(beerOrder.getOrderStatus()).isEqualTo(OrderStatusEnum.VALIDATION_PENDING);
+    verify(jmsTemplate).convertAndSend(anyString(), any(ValidateBeerOrderRequest.class));
   }
 }
