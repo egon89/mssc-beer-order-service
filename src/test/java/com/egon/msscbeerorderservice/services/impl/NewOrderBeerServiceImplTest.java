517c8d9980893426ace2d8db8abb45b9c223d09a
Everton Goncalves <everton.goncalves.89@gmail.com>
2024-04-22 22:55:21 -0300

    test: wire mock for http request

--- src/test/java/com/egon/msscbeerorderservice/services/impl/NewOrderBeerServiceImplTest.java
+++ src/test/java/com/egon/msscbeerorderservice/services/impl/NewOrderBeerServiceImplTest.java
@@ -1,12 +1,19 @@
 package com.egon.msscbeerorderservice.services.impl;
 
+import com.egon.brewery.dtos.BeerDto;
 import com.egon.brewery.dtos.BeerOrderDto;
 import com.egon.brewery.dtos.BeerOrderLineDto;
 import com.egon.brewery.dtos.events.ValidateBeerOrderRequest;
 import com.egon.msscbeerorderservice.enums.OrderStatusEnum;
 import com.egon.msscbeerorderservice.repositories.BeerOrderRepository;
-import com.egon.msscbeerorderservice.services.GetBeerByUpcService;
 import com.egon.msscbeerorderservice.services.NewOrderBeerService;
+import com.egon.msscbeerorderservice.utils.BeerIntegrationUtil;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.maciejwalkowiak.wiremock.spring.ConfigureWireMock;
+import com.maciejwalkowiak.wiremock.spring.EnableWireMock;
+import com.maciejwalkowiak.wiremock.spring.InjectWireMock;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,13 +25,23 @@ import java.util.List;
 import java.util.Random;
 import java.util.UUID;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.okJson;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.verify;
 
+// https://wiremock.org/docs/solutions/spring-boot/
+// https://github.com/maciejwalkowiak/wiremock-spring-boot
 @SpringBootTest
+@EnableWireMock({
+    @ConfigureWireMock(name = "beer-service", property = "beer.service.host")
+})
 class NewOrderBeerServiceImplTest {
+
+  @InjectWireMock("beer-service")
+  private WireMockServer wiremock;
 
   @Autowired
   NewOrderBeerService newOrderBeerService;
@@ -33,17 +50,20 @@ class NewOrderBeerServiceImplTest {
   BeerOrderRepository repository;
 
   // remove the jmsTemplate mock to run an integration test
-  // @MockBean
+  @MockBean
   JmsTemplate jmsTemplate;
 
-  @MockBean
-  private GetBeerByUpcService getBeerByUpcService;
+  @Autowired
+  ObjectMapper objectMapper;
 
   @Test
-  void shouldCreateANewBeerOrder() {
+  void shouldCreateANewBeerOrder() throws JsonProcessingException {
+    final var upc = String.valueOf(new Random().nextInt(50000));
+    final var path = "%s/upc/%s".formatted(BeerIntegrationUtil.BEER_PATH, upc);
+    final var beerResponse = BeerDto.builder().id(UUID.randomUUID()).upc(upc).name("Beer A").build();
     final var orderLine = BeerOrderLineDto.builder()
         .beerId(UUID.randomUUID())
-        .upc(String.valueOf(new Random().nextInt(50000)))
+        .upc(upc)
         .price(new BigDecimal(new Random().nextLong(10)))
         .build();
     final var dto = BeerOrderDto.builder()
@@ -51,6 +71,9 @@ class NewOrderBeerServiceImplTest {
         .orderStatus(OrderStatusEnum.NEW)
         .beerOrderLines(List.of(orderLine))
         .build();
+    wiremock.stubFor(get(path)
+        .willReturn(okJson(objectMapper.writeValueAsString(beerResponse))));
+
     final var savedBeerOrder = newOrderBeerService.execute(dto);
     assertThat(savedBeerOrder.getOrderStatus()).isEqualTo(OrderStatusEnum.NEW);
 
